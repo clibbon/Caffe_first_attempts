6a0c4914d36564cf182a86c952bc76c8a7f38579
Alex Clibbon <alex.clibbon@gmail.com>
2015-05-19 00:46:54 +0100

    File size checker added

---
+++ tools/check_file_sizes.py
@@ -0,0 +1,12 @@
+'''Check images walks through the images, loads them, and checks that 
+the dimensions match the given dimensions'''
+from PIL import Image
+imgList = open('../MultitaskModel/singlelabels.txt')
+desired_size = 227 # The size images should be
+
+for line in imgList:
+    words = line.split()
+    tmp = Image.open('/data/ad6813/Nus_wide/imgs_resized/' + words[0])
+    (w, h) = tmp.size
+    if w != desired_size | h != desired_size:
+        print "Image {} is incorrect".format(words[0])
