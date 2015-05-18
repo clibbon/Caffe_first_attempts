4786f2a00c10f59d130f3254385e616b0b090ae2
Alex Clibbon <alex.clibbon@gmail.com>
2015-05-18 17:34:12 +0100

    Working image preprocess

---
+++ tools/my_resize.py
@@ -0,0 +1,41 @@
+from PIL import Image
+
+def resize(img, box, fit, out):
+    '''Downsample the image.
+    A second version of this was made, which uses resize rather than the 
+    thumbnail function, since this this won't upsize images. Function taken
+    from http://united-coders.com/christian-harms/image-resizing-tips-every-coder-should-know/
+    '''
+    #preresize image with factor 2, 4, 8 and fast algorithm
+    factor = 1
+    while img.size[0]/factor > 2*box[0] and img.size[1]*2/factor > 2*box[1]:
+        factor *=2
+    if factor > 1:
+        img.thumbnail((img.size[0]/factor, img.size[1]/factor), Image.NEAREST)
+
+    #calculate the cropping box and get the cropped part
+    if fit:
+        x1 = y1 = 0
+        x2, y2 = img.size
+        wRatio = 1.0 * x2/box[0]
+        hRatio = 1.0 * y2/box[1]
+        if hRatio > wRatio:
+            y1 = int(y2/2-box[1]*wRatio/2)
+            y2 = int(y2/2+box[1]*wRatio/2)
+        else:
+            x1 = int(x2/2-box[0]*hRatio/2)
+            x2 = int(x2/2+box[0]*hRatio/2)
+        img = img.crop((x1,y1,x2,y2))
+
+    #Resize the image with best quality algorithm ANTI-ALIAS. If the image is 
+    # larger then use the thumbnail as per
+    if all([x > box[0] for x in img.size]):
+        img.thumbnail(box, Image.ANTIALIAS)
+        print 'I used thumbnail'
+    else:
+        img = img.resize(box, Image.ANTIALIAS)
+
+    #save it into a file-like object
+    img.save(out, "JPEG", quality=75)
+    return img
+#resize
---
+++ tools/resize_pictures.py
@@ -0,0 +1,23 @@
+'''For pre-processing all images for use in the training 
+network. All images will be resized to 227*227, and 
+cropped if need be '''
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Resize and crop images.')
+parser.add_argument('--source', dest = 'source_folder', action='store',
+                    default='.')
+parser.add_argument('--dest', dest = 'destination_folder', action='store',
+                    default='./resized/')
+args = parser.parse_args()
+resize_dims = (227, 227) # Number of pixels in resized image
+import os
+from PIL import Image
+from my_resize import resize
+
+filenames = os.listdir(args.source_folder)
+
+# Loop through all images and resize
+for filename in filenames:
+    img = Image.open(args.source_folder + '/'+ filename)
+    resize(img, resize_dims, True, args.destination_folder + '/' + filename)
