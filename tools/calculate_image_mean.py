96af586b35d26e0c0c5ae6e5c34b6f6385b03d2c
Alex Clibbon <alex.clibbon@gmail.com>
2015-05-29 21:21:22 +0100

    updated image mean calculation

--- tools/calculate_image_mean.py
+++ tools/calculate_image_mean.py
@@ -3,10 +3,15 @@ channel, and saves them in npy format, in the order BGR """
 
 from PIL import Image
 import numpy as np
+caffe_root = '/home/alex/Caffe/caffe/'
+import sys
+sys.path.insert(0, caffe_root + 'python')
+from caffe import io
+
 path = '/home/alex/Documents/Caffe_First_Attempt/'
 imageSet = path + 'resized'
 imageLabels = path + 'temp/test_labels.txt'
-destination = path + 'temp/img_mean.npy'
+destination = path + 'temp/img_mean.binaryproto'
 img_size = (227, 227)
 img_mean = np.zeros((3,img_size[0],img_size[1]))
 imageFile = open(imageLabels,'r')
@@ -17,4 +22,8 @@ for line in imageFile:
     img_mean += np.array(img).transpose(2,0,1)
     nImages += 1
 img_mean = img_mean/nImages
-np.save(destination, img_mean)
+img_mean = img_mean[[2,1,0],:,:]
+meanBlob = io.array_to_blobproto(np.expand_dims(img_mean,0))
+toSave = open(destination,'wb')
+toSave.write(meanBlob.SerializeToString())
+toSave.close()
